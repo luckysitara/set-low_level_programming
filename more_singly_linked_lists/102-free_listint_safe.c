827ac8739bec5b2bb69f7d08d167b2896978eff4
luckysitara <udoessienprecious@gmail.com>
2026-05-24 02:49:16 -0700

    return

--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -5,27 +5,27 @@
  * free_listint_safe - Frees a listint_t list safely (handles loops)
  * @h: double pointer to the head of the list
  *
- * Return: the size of the list that was freed
+ * Return: size of the list freed
  */
 size_t free_listint_safe(listint_t **h)
 {
-    size_t c = 0;
-    listint_t *current, *next;
+	size_t c = 0;
+	listint_t *current, *next;
 
-    if (!h || !*h)
-        return (0);
+	if (!h || !*h)
+		return (0);
 
-    current = *h;
-    while (current)
-    {
-        next = current->next;  /* save next BEFORE freeing */
-        free(current);
-        c++;
-        if (next <= current)   /* loop detected or end reached */
-            break;
-        current = next;
-    }
+	current = *h;
+	while (current)
+	{
+		next = current->next;   /* save next BEFORE freeing */
+		free(current);
+		c++;
+		if (next <= current)    /* loop: next points backward = done */
+			break;
+		current = next;
+	}
 
-    *h = NULL;                 /* set head to NULL as required */
-    return (c);
+	*h = NULL;
+	return (c);
 }
