64fd7c12cfd510a03fc9f62dc95e68ebf906dcbb
luckysitara <udoessienprecious@gmail.com>
2026-05-24 02:55:41 -0700

    return

--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -2,53 +2,59 @@
 #include <stdlib.h>
 
 /**
+* count_nodes - counts nodes in a list (handles loops)
+* @h: pointer to head of list
+* Return: total number of unique nodes
+*/
+static size_t count_nodes(listint_t *h)
+{
+	listint_t *s, *f;
+	size_t count = 0;
+
+	s = f = h;
+	while (f && f->next)
+	{
+		s = s->next;
+		f = f->next->next;
+		if (s != f)
+			continue;
+		s = h;
+		while (s != f)
+		{
+			s = s->next;
+			f = f->next;
+			count++;
+		}
+		f = f->next;
+		count++;
+		while (f != s)
+		{
+			f = f->next;
+			count++;
+		}
+		return (count);
+	}
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
+
+/**
 * free_listint_safe - Frees a listint_t list safely (handles loops)
 * @h: double pointer to the head of the list
 * Return: size of the list freed
 */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *s, *f, *cur, *tmp;
-	size_t count = 0, i;
+	listint_t *cur, *tmp;
+	size_t count, i;
 
 	if (!h || !*h)
 		return (0);
-
-	s = f = *h;
-	while (f && f->next)
-	{
-		s = s->next;
-		f = f->next->next;
-		if (s == f)		/* loop found */
-		{
-			/* count nodes: linear + loop */
-			s = *h;
-			while (s != f)
-			{
-				s = s->next;
-				f = f->next;
-				count++;
-			}
-			/* count loop length */
-			f = f->next;
-			count++;
-			while (f != s)
-			{
-				f = f->next;
-				count++;
-			}
-			goto free_nodes;
-		}
-	}
-	/* no loop: count normally */
-	cur = *h;
-	while (cur)
-	{
-		count++;
-		cur = cur->next;
-	}
-
-free_nodes:
+	count = count_nodes(*h);
 	cur = *h;
 	for (i = 0; i < count; i++)
 	{
