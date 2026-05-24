25afdc5943b19ec7a2dcb5ab44a8ccb5b11548ac
luckysitara <udoessienprecious@gmail.com>
2026-05-24 02:53:56 -0700

    return

--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -2,30 +2,60 @@
 #include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a listint_t list safely (handles loops)
- * @h: double pointer to the head of the list
- *
- * Return: size of the list freed
- */
+* free_listint_safe - Frees a listint_t list safely (handles loops)
+* @h: double pointer to the head of the list
+* Return: size of the list freed
+*/
 size_t free_listint_safe(listint_t **h)
 {
-	size_t c = 0;
-	listint_t *current, *next;
+	listint_t *s, *f, *cur, *tmp;
+	size_t count = 0, i;
 
 	if (!h || !*h)
 		return (0);
 
-	current = *h;
-	while (current)
+	s = f = *h;
+	while (f && f->next)
 	{
-		next = current->next;   /* save next BEFORE freeing */
-		free(current);
-		c++;
-		if (next <= current)    /* loop: next points backward = done */
-			break;
-		current = next;
+		s = s->next;
+		f = f->next->next;
+		if (s == f)		/* loop found */
+		{
+			/* count nodes: linear + loop */
+			s = *h;
+			while (s != f)
+			{
+				s = s->next;
+				f = f->next;
+				count++;
+			}
+			/* count loop length */
+			f = f->next;
+			count++;
+			while (f != s)
+			{
+				f = f->next;
+				count++;
+			}
+			goto free_nodes;
+		}
+	}
+	/* no loop: count normally */
+	cur = *h;
+	while (cur)
+	{
+		count++;
+		cur = cur->next;
 	}
 
+free_nodes:
+	cur = *h;
+	for (i = 0; i < count; i++)
+	{
+		tmp = cur->next;
+		free(cur);
+		cur = tmp;
+	}
 	*h = NULL;
-	return (c);
+	return (count);
 }
